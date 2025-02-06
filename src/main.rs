cbcbc818a5e8afac4e96fc815639f0718360a462
mikku@arch <michael.lindemans@outlook.com>
2025-02-06 22:36:20 +0100

    update the main func to have the basic struct implementation

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod generator;
 
 fn main() {
     let generator = Generator {
-        cube_count: 5,
-        metadata: Metadata { cube: vec![] },
+        cube_count: 0,
+        metadata: Some(Metadata { cubes: vec![] }),
     };
 
     println!("Hello, world!");
