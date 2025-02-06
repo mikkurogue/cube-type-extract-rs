64917d6f573875d80d92eafa642166497cc759ff
mikku@arch <michael.lindemans@outlook.com>
2025-02-06 22:08:48 +0100

    add serde. finish configuration logic

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,52 @@
+use colored::Colorize;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 pub struct Configuration {
     pub cube_url: String,
     pub output: String,
     pub file_name: String,
-    pub prefix_list: Vec<Prefix>,
-    pub ignore_error_cubes: bool,
+    pub prefixes: Vec<Prefix>,
+    pub ignore_error_cube: bool,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Prefix {
     pub name: String,
     pub prefix: String,
 }
 
 pub fn generate_default_config() {
-    // TODO
+    let default_config = Configuration {
+        cube_url: "http://localhost:4000/cubejs.api".to_owned(),
+        output: "./".to_owned(),
+        file_name: "cubejs-types".to_owned(),
+        prefixes: vec![Prefix {
+            name: "Placeholder".to_owned(),
+            prefix: "Main".to_owned(),
+        }],
+        ignore_error_cube: true,
+    };
+
+    let json_data = match serde_json::to_string_pretty(&default_config) {
+        Ok(json) => json,
+        Err(err) => {
+            eprintln!("{}, {}", "Could not marshal config file: ".red(), err);
+            std::process::exit(1);
+        }
+    };
+
+    let filename = "type-gen-config.json";
+
+    if let Err(err) = std::fs::write(filename, json_data) {
+        eprintln!("{} {}", "Could not write to file: ".red(), err);
+        std::process::exit(1);
+    }
+
+    println!(
+        "{}",
+        "Successfully created the default configuration file!".green()
+    );
 }
 
 pub fn validate_configuration() -> bool {
@@ -20,16 +54,10 @@ pub fn validate_configuration() -> bool {
 }
 
 pub fn read() -> Result<Configuration, std::io::Error> {
-    // TODO: read the file and parse it here.
+    let data = std::fs::read_to_string("type-gen-config.json")?;
 
-    Ok(Configuration {
-        cube_url: "".to_owned(),
-        output: "".to_owned(),
-        file_name: "".to_owned(),
-        prefix_list: vec![Prefix {
-            name: "".to_owned(),
-            prefix: "".to_owned(),
-        }],
-        ignore_error_cubes: true,
-    })
+    let config: Configuration = serde_json::from_str(&data)
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+
+    Ok(config)
 }
